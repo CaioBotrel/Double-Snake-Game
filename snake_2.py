b566828f35d3fcfae897dc2a2d7bb3e6d4107d54
Caio Botrel Bueno <101948497+CaioBotrel@users.noreply.github.com>
2023-02-01 14:42:37 -0300

    Add files via upload
    This new patch deleted the second snake and added a speed boost every time the snake eats the apple. It's getting faster...

--- snake_2.py
+++ snake_2.py
@@ -22,49 +22,61 @@ def random_on_grid():
 pygame.init()
 screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption('Snake')
+# Create a font object using pygame.font module
+myfont = pygame.font.SysFont("Arial", 30)
+# Render the "Game Over" text
+gameover_text = myfont.render("Game Over", True, (255, 0, 0))
+
 
 snake_pos = [(250, 50), (260, 50), (270, 50)]
 snake_surface = pygame.Surface((PIXEL_SIZE, PIXEL_SIZE))
-snake_surface.fill((255, 255, 255))
+snake_surface.fill((127, 255, 0))
 snake_direction = K_LEFT
-
+snake_pos2 = [(300, 250), (310, 250), (320, 250)]
 
 apple_surface = pygame.Surface((PIXEL_SIZE, PIXEL_SIZE))
 apple_surface.fill((255, 0, 0))
 apple_pos = random_on_grid()
 
+speed = 15
+speed_increment = 5
 
 while True:
-    pygame.time.Clock().tick(15)
+    pygame.time.Clock().tick(speed)
     screen.fill((0,0,0))
     for event in pygame.event.get():
-     if event.type == QUIT:
-        pygame.quit()
-        quit()
-
-     elif event.type == KEYDOWN:
-        if event.key in [K_UP, K_DOWN, K_LEFT, K_RIGHT]:
-            snake_direction = event.key
-
+        if event.type == QUIT:
+            break
+        elif event.type == KEYDOWN:
+            if event.key in [K_UP, K_DOWN, K_LEFT, K_RIGHT]:
+                snake_direction = event.key
+            
     screen.blit(apple_surface, apple_pos)
+    pygame.display.update()
 
     if collision(apple_pos, snake_pos[0]):
         snake_pos.append((-10, -10))
         apple_pos = random_on_grid()
-
+        speed += speed_increment
 
     for pos in snake_pos:
         screen.blit(snake_surface, pos)
-
         screen.blit(apple_surface, apple_pos)
-    
+
     for i in range(len(snake_pos)-1, 0, -1):
         if collision(snake_pos[0], snake_pos[i]):
+            # Blit the "Game Over" text to the screen
+            screen.blit(gameover_text, (WINDOW_SIZE[0]/2 - 100, WINDOW_SIZE[1]/2))
+            pygame.display.update()
+            pygame.time.wait(3000) # wait for 3 seconds
             pygame.quit()
             quit()
         snake_pos[i] = (snake_pos[i-1])
     
     if off_limits(snake_pos[0]):
+        # Blit the "Game Over" text on the screen
+        screen.blit(gameover_text, (WINDOW_SIZE[0/2] - 100, WINDOW_SIZE[1/2]))
+        pygame.display.update
         pygame.quit()
         quit()
 
@@ -77,6 +89,8 @@ while True:
     elif snake_direction == K_RIGHT:
         snake_pos[0] = (snake_pos[0][0] + PIXEL_SIZE, snake_pos[0][1])
 
+    if off_limits(snake_pos2[0]):
+        pygame.quit()
+        quit()
 
     pygame.display.update()
-    
